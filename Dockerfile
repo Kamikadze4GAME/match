b0af7dfb7adad784e82df0e81e9423cb5e8fd1cd
Oliver Rice <github@oliverrice.com>
2019-03-25 12:58:57 -0500

    minify dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,19 @@
-FROM python:3.6
-MAINTAINER Alex Kern <alex@distributedsystems.com>
+FROM python:3.6.8-slim
+MAINTAINER Oliver Rice <oliver@truepic.com>
 
 RUN apt-get update && \
-    apt-get install -y libopenblas-dev gfortran && \
-    pip install numpy==1.12.1 && \
-    pip install scipy==0.19.0 && \
-    pip install gunicorn==19.7.1 && \
-    pip install flask==0.12.2 && \
-    pip install image-match==1.1.2 && \
-    pip install 'elasticsearch>=6.0.0,<7.0.0' && \
-    rm -rf /var/lib/apt/lists/*
+    apt-get install -y libopenblas-dev gfortran
 
-COPY server.py wait-for-it.sh /
+COPY server.py setup.py /
+
+RUN pip3.6 install "numpy==1.16.2"
+RUN pip3.6 install -e ".[prod]"
+
 
 EXPOSE 80
 ENV PORT=80 \
     WORKER_COUNT=4 \
-    ELASTICSEARCH_URL=elasticsearch:9200 \
+    ELASTICSEARCH_URL=https://vpc-match-hmyezdubn6xpxprx5txxrfopde.us-east-1.es.amazonaws.com \
     ELASTICSEARCH_INDEX=images \
     ELASTICSEARCH_DOC_TYPE=images \
     ALL_ORIENTATIONS=true
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="match",
@@ -14,30 +14,31 @@ setup(
         "Programming Language :: Python :: 3.6",
     ],
     python_requires=">=3.6",
+    packages=find_packages("src/main/python"),
+    package_dir={"": "src/main/python"},
+    entry_points={"console_scripts": ["match = app:app"]},
     test_suite="nose.collector",
     install_requires=[
         "numpy==1.16.2",
         "scipy==0.19.0",
         "flask==1.0.2",
         "image-match==1.1.2",
+        "flasgger==0.9.2",
         "python-dotenv==0.10.1",
         "certifi==2019.3.9",
-        "elasticsearch==6.3.1",
+        "elasticsearch==2.3.0",
     ],
     extras_require={
         "dev": [
-            "matplotlib==3.0.2",
+            "pre-commit==1.14.4",
             "black==18.9b0",
             "isort==4.3.4",
             "pylint==2.2.2",
             "pytest==4.0.1",
+            "autoflake==1.2",
             "nose==1.3.7",
             "mypy==0.650",
         ],
         "prod": ["gunicorn==19.9.0"],
     },
 )
-
-def run(self):
-    __builtins__.__NUMPY_SETUP__ = False
-    import numpy
